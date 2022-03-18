168e6906c5f10f907155594e1830f339bbfeb006
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-03-18 21:13:08 +0000

    test for getTotalValue() done

--- lab2/lab1_2/src/test/java/com/tqs/StocksPortofolioTest.java
+++ lab2/lab1_2/src/test/java/com/tqs/StocksPortofolioTest.java
@@ -1,8 +1,12 @@
 package com.tqs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -13,5 +17,18 @@ public class StocksPortofolioTest {
     @InjectMocks
     StocksPortofolio portofolio;
 
-    
+    @Test
+    public void whenGetTotalValue_SumWithMockedValues() {
+        IStockmarketService mockMarket = Mockito.mock(IStockmarketService.class);
+        StocksPortofolio portofolio = new StocksPortofolio(mockMarket);
+
+        Mockito.when(mockMarket.lookuUpPrice("AAPL")).thenReturn(4.0);
+        Mockito.when(mockMarket.lookuUpPrice("EBAY")).thenReturn(2.0);
+
+        portofolio.addStock(new Stock("EBAY",2));
+        portofolio.addStock(new Stock("AAPL", 3));
+
+        assertEquals(16.0, portofolio.getTotalValue());
+        Mockito.verify(mockMarket, Mockito.times(2)).lookuUpPrice(Mockito.anyString());
+    }
 }
