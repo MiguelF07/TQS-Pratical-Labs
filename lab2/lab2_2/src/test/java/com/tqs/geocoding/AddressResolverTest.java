f09c988817339c24ac4aa087018623e798833835
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-03-21 12:02:36 +0000

    2.2 done

--- lab2/lab2_2/src/test/java/com/tqs/geocoding/AddressResolverTest.java
+++ lab2/lab2_2/src/test/java/com/tqs/geocoding/AddressResolverTest.java
@@ -12,6 +12,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -19,27 +21,47 @@ import static org.mockito.ArgumentMatchers.anyString;
 @ExtendWith(MockitoExtension.class)
 class AddressResolverTest {
 
+    @Mock
+    ISimpleHttpClient mockClient;
+
+    @InjectMocks
+    AddressResolver aR;
+
 
     @Test
     void whenResolveAlboiGps_returnCaisAlboiAddress() throws ParseException, IOException, URISyntaxException {
 
-        //todo
+        //Service to mock
+        mockClient = Mockito.mock(ISimpleHttpClient.class);
 
-        //e.g.
-        // Optional<Address> result = resolver.findAddressForLocation(40.640661, -8.656688);
+        //SuT with injected mock
+        aR = new AddressResolver(mockClient);
 
-        //return
-        // assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
+        String feedback = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\u00A9 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\u00A9 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.640661,\"lng\":-8.656688}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"displayLatLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.64052368145179,-8.656712986761146|marker-sm-50318A-1&scalebar=true&zoom=15&rand=533918926\"}]}]}";
+
+        Mockito.when(mockClient.doHttpGet(anyString())).thenReturn(feedback);
+
+        Optional<Address> result = aR.findAddressForLocation(40.640661, -8.656688);
+
+        assertEquals( result.get(), new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
 
     }
 
     @Test
     public void whenBadCoordidates_thenReturnNoValidAddress() throws IOException, URISyntaxException, ParseException {
 
+
+        String feedback = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\u00A9 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\u00A9 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":4589258.0,\"lng\":2.98459245E8}},\"locations\":[]}]}";
         //todo
 
+        mockClient = Mockito.mock(ISimpleHttpClient.class);
+        aR = new AddressResolver(mockClient);
+
+        Mockito.when(mockClient.doHttpGet(anyString())).thenReturn(feedback);
         //e.g.
-        //  Optional<Address> result = resolver.findAddressForLocation(-300, -810);
+        Optional<Address> result = aR.findAddressForLocation(4589258,298459245);
+
+        assertThrows(NoSuchElementException.class, () -> result.get());
 
     }
 }
