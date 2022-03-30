e0f96e6624d1e59a3ad0ffa166bc3d717096ba8e
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-03-30 23:15:02 +0100

    lab3 done

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ TQS (Software Quality Testing) Individual Classes Portfolio .
 This is the repository where I'll keep my TQS Pratical Guides updated and ready to evaluate.
 
 - Lab 1 - Complete
-- Lab 2
+- Lab 2 - Complete
+- Lab 3 - Complete
 
 Miguel Ferreira 98599
 
---
+++ lab3/lab3_3/src/test/java/com/tqs/lab3_2/CarRestControllerTemplateITTest.java
@@ -0,0 +1,62 @@
+package com.tqs.lab3_2;
+
+import java.util.List;
+
+import com.tqs.lab3_2.Model.Car;
+import com.tqs.lab3_2.Repository.CarRepository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+@TestPropertySource( locations = "./car/application-integrationtest.properties")
+public class CarRestControllerTemplateITTest {
+    @LocalServerPort
+    int randomServerPort;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private CarRepository carRep;
+
+    @AfterEach
+    public void resetDb() {
+        carRep.deleteAll();
+    }
+
+    @Test
+    void whenValidInput_thenCreateCar() {
+        Car car1 = new Car("Mercedes-Benz","Classe A");
+        ResponseEntity<Car> entity = restTemplate.postForEntity("/api/cars", car1, Car.class);
+
+        List<Car> found = carRep.findAll();
+        assertThat(found).extracting(Car::getModel).containsOnly("Classe A");
+    }
+
+    @Test
+    void givenCars_whenGetCars_thenStatus200() {
+        Car car1 = new Car("Mercedes-Benz","Classe A");
+        Car car2 = new Car("BMW","Serie 1");
+        carRep.saveAndFlush(car1);
+        carRep.saveAndFlush(car2);
+
+        ResponseEntity<List<Car>> response = restTemplate.exchange("/api/cars", HttpMethod.GET,null, new ParameterizedTypeReference<List<Car>>() {});
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).extracting(Car::getModel).containsExactly("Classe A","Serie 1");
+    }
+}
