f54b9e4e9c8f1b22e4101485e9a864e25e684e3a
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-05-02 01:14:08 +0100

    added logging to last test

--- HW1/backend/src/test/java/com/tqs/homework1/CovidPlatformSteps.java
+++ HW1/backend/src/test/java/com/tqs/homework1/CovidPlatformSteps.java
@@ -17,6 +17,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CovidPlatformSteps {
 
@@ -29,6 +31,8 @@ public class CovidPlatformSteps {
         driver = new FirefoxDriver(options);
 //        driver = new FirefoxDriver();
         driver.get(url);
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I navigate to");
+
     }
     @When("I choose {string} as the country I want to see")
     public void i_choose_as_the_country_i_want_to_see(String string) {
@@ -38,6 +42,7 @@ public class CovidPlatformSteps {
             WebElement dropdown = driver.findElement(By.cssSelector("select"));
             dropdown.findElement(By.xpath("//option[. = \'"+string+"\']")).click();
         }
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I choose as the country i want to see");
 
     }
     @Then("I should see the {string} modal")
@@ -45,6 +50,7 @@ public class CovidPlatformSteps {
         wait = new WebDriverWait(driver,5);
         wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/h4")),string));
         assertThat(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/h4")).getText()).contains(string);
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I should see the modal");
     }
 
     @Then("I should see the COVID data for the country {string}")
@@ -57,6 +63,7 @@ public class CovidPlatformSteps {
         //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[2]/div/div/b[1]")));
         driver.findElement(By.cssSelector(".row:nth-child(1) > .col")).click();
         assertThat(driver.findElement(By.cssSelector(".row:nth-child(1) > .col")).getText()).contains(string);
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I should see the covid data for the country");
     }
 
     @When("I enter the From date as {string}")
@@ -68,6 +75,7 @@ public class CovidPlatformSteps {
         from.click();
         from.sendKeys(string);
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/h2")).click(); //To dismiss the datepicker
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I enter the from date");
     }
 
     @When("I enter the To date as {string}")
@@ -79,11 +87,13 @@ public class CovidPlatformSteps {
         from.click();
         from.sendKeys(string);
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/h2")).click(); //To dismiss the datepicker
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I enter the to date");
     }
     @When("I click the {string} button")
     public void i_click_the_button(String string) {
         assertThat(driver.findElement(By.cssSelector(".btn")).getText()).isEqualTo("See History");
         driver.findElement(By.cssSelector(".btn")).click();
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I click the button");
     }
     @Then("I should see the day {string}")
     public void i_should_see_the_day(String string) {
@@ -91,5 +101,6 @@ public class CovidPlatformSteps {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[2]/div/div/b[1]")));
         driver.findElement(By.cssSelector(".row:nth-child(1) > .col")).click();
         assertThat(driver.findElement(By.cssSelector(".row:nth-child(1) > .col")).getText()).contains(string);
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "I should see the day");
     }
 }
